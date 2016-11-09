47cd9d393cd38cc42e77895e99febff44eca8c86
Lucas Vieira <vieiralucas4@gmail.com>
2016-11-09 19:30:35 -0200

    Create WorkInterval and Project

--- server.js
+++ server.js
@@ -1,6 +1,16 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const Bluebird = require('bluebird');
+const router = require('./lib/router');
+
+mongoose.Promise = Bluebird;
+mongoose.connect('mongodb://localhost/zilean');
 
 const app = express();
+
+app.use(bodyParser.json());
+app.use(router);
 
 app.set('port', (process.env.API_PORT || 3001));
 
